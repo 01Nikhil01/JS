5e905aefdedee71132d8b0ff998f1d359299c9a4
ashok_suvvada <suvvadashok@gmail.com>
2024-02-22 19:16:47 +0530

    add bar apex

--- src/App.jsx
+++ src/App.jsx
@@ -3,13 +3,14 @@ import "@/assets/css/style.css";
 import AdminLayout from "./layouts/AdminLayout";
 import BackToTop from "./layouts/BackToTop";
 import { LayoutProvider } from "./context/useLayoutContext";
+import BarApex from "./page/bargraph";
 
 const App = () => {
   return (
     <LayoutProvider>
       <AdminLayout>
         <BackToTop />
-        {/* component */}
+        <BarApex />
       </AdminLayout>
     </LayoutProvider>
   );
