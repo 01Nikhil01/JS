4d79ae4ffd77be200eb568a3aee09534aa0b613c
srikanth <srikanthgummadi516@gmail.com>
2024-02-22 17:30:33 +0530

    add admin add customer page

--- src/App.jsx
+++ src/App.jsx
@@ -3,13 +3,14 @@ import "@/assets/css/style.css";
 import AdminLayout from "./layouts/AdminLayout";
 import BackToTop from "./layouts/BackToTop";
 import { LayoutProvider } from "./context/useLayoutContext";
+import AddCustomer from "./components/AddCustomer/AddCustomer";
 
 const App = () => {
   return (
     <LayoutProvider>
       <AdminLayout>
         <BackToTop />
-        {/* component */}
+        <AddCustomer />      
       </AdminLayout>
     </LayoutProvider>
   );
