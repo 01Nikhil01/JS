6876069db198adff6c8ea9c097600c0b04e40487
ashok_suvvada <suvvadashok@gmail.com>
2024-02-22 13:34:02 +0530

    fix vertical menu

--- src/components/SimplebarReactClient.jsx
+++ src/components/SimplebarReactClient.jsx
@@ -1,7 +1,6 @@
 import SimpleBar from "simplebar-react";
 
 const SimplebarReactClient = ({ children, ...options }) => {
-  console.log(children);
   return <SimpleBar {...options}>{children}</SimpleBar>;
 };
 
