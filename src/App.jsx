7d09d780f8d0405118eadf619a8f8008cd6c6e8d
ashok_suvvada <suvvadashok@gmail.com>
2024-02-22 17:02:18 +0530

    add area apex chart

--- src/App.jsx
+++ src/App.jsx
@@ -3,13 +3,14 @@ import "@/assets/css/style.css";
 import AdminLayout from "./layouts/AdminLayout";
 import BackToTop from "./layouts/BackToTop";
 import { LayoutProvider } from "./context/useLayoutContext";
+import AreaApex from "./page/page";
 
 const App = () => {
   return (
     <LayoutProvider>
       <AdminLayout>
         <BackToTop />
-        {/* component */}
+        <AreaApex />
       </AdminLayout>
     </LayoutProvider>
   );
